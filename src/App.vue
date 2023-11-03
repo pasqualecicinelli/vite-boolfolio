fe5e007552d427a7d65ff43e17e024dc80612a67
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-11-03 18:05:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+
+  created() {
+    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+      console.log(response.data.data);
+      this.projects = response.data.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <h1>hello</h1>
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import "./assets/scss/style.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
