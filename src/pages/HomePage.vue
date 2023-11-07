d230a40fd19a192b600d26791cf681ab7adfb150
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-11-07 16:54:43 +0100

    Milestone 2 create home, portfolio and proj-detail

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h1 class="text-center my-2">Home Page</h1>
+</template>
+
+<style lang="scss"></style>
--- src/router.js
+++ src/router.js
@@ -3,13 +3,34 @@ import { createRouter, createWebHistory } from "vue-router";
 //Importo i componenti
 
 import ProjectList from "./components/projects/ProjectList.vue";
+import HomePage from "./pages/HomePage.vue";
+import PortfolioPage from "./pages/PortfolioPage.vue";
+import ProjectDetailPage from "./pages/ProjectDetailPage.vue";
 
 const router = createRouter({
   //aggiungo history e rotte
 
   history: createWebHistory(),
 
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "homepage",
+      component: HomePage,
+    },
+
+    {
+      path: "/portfolio",
+      name: "portfolio",
+      component: PortfolioPage,
+    },
+
+    {
+      path: "/project-detail",
+      name: "project-detail",
+      component: ProjectDetailPage,
+    },
+  ],
 });
 
 export { router };
