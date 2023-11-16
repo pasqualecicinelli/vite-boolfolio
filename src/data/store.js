1f3de65c361886739d9b7539cedf78e208bb2a2a
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-11-16 16:58:39 +0100

    change baseUrl for show the img

--- src/data/store.js
+++ src/data/store.js
@@ -2,6 +2,9 @@ import { reactive } from "vue";
 
 export const store = reactive({
   api: {
-    baseUrl: "http://127.0.0.1:8000/api/",
+    baseUrl: "http://localhost:8000/api/",
+
+    /*Uso "http://localhost:8000/api/" per vedere le img
+    Altrimenti uso http://127.0.0.1:8000/api */
   },
 });
